110437d19873add2360325f051581ea7080d678c
Owethu Novuka <95959916+onovuka@users.noreply.github.com>
2023-02-25 16:55:03 +0200

    Create list.py
    program contains function that will list available files

---
+++ list.py
@@ -0,0 +1,27 @@
+# listing all the available files
+
+import mysql.connector
+
+# returns filename + date + type + type + protection
+
+db = mysql.connector.connect(
+    host="",
+    user="",
+    password="",
+    database=""
+)
+
+cursor = db.cursor()
+
+
+def return_list():
+    query = "SELECT filepath, upload_date, type, access FROM Files WHERE email = %s AND password = %s"
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+
