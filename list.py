32d617294b132e560d1e4aeb7279ad8a6e14e8b3
Katlego-Sekoele <sekoelekatlego@gmail.com>
2023-02-25 22:24:10 +0200

    Updated list.py to make use of database_manager

--- list.py
+++ list.py
@@ -1,28 +1,11 @@
 # listing all the available files
 
-import mysql.connector
-
-# returns filename + date + type + type + protection
-
-db = mysql.connector.connect(
-    host="",
-    user="",
-    password="",
-    database=""
-)
-
-cursor = db.cursor()
+import database_manager as database
 
 
 def return_list():
-    # name of the database has been changed to resources !
-    query = "SELECT filepath, upload_date, type, access FROM Files WHERE email = %s AND password = %s"
+    query = "SELECT resource_path, upload_date, type, public FROM Resources"
 
-    cursor.execute(query)
+    rows = database.query(query)
 
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-
+    return rows
