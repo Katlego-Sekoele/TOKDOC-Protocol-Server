075cf9d495dca670319738296ca41e2eafe837cd
Katlego-Sekoele <sekoelekatlego@gmail.com>
2023-02-27 12:08:18 +0200

    Updated documentation

--- src/RequestHandlers/list.py
+++ src/RequestHandlers/list.py
@@ -1,10 +1,22 @@
-# listing all the available files
-
-
+"""
+ listing all the available files
+"""
 from Utilities import database_manager as database
 
 
 def return_list(email: str) -> list[dict]:
+    """
+    A method that will return a list of files that are public or that the user owns
+    :param email:
+    :return: dict -> {
+        'resource_id': int,
+        'type': str,
+        'resource_path': str,
+        'upload_date': datetime.datetime(),
+        'user_id': int,
+        'public': bool
+    }
+    """
 
     users_query = "SELECT * FROM Users WHERE email = %s"
     users_parameters = (email,)
@@ -20,5 +32,8 @@ def return_list(email: str) -> list[dict]:
         rows = database.query(resources_query, resources_parameters)
     else:
         rows = database.query(resources_query)
+
+    for row in rows:
+        row['public'] = row['public'] == 1
 
     return rows
